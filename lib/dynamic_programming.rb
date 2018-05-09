7a00cfe474afe0b6841f967b41e515c7dd056c89
Kevin Yee <kevinyee1993@gmail.com>
2018-05-09 10:42:03 -0700

    Finish frog cache builder

--- lib/dynamic_programming.rb
+++ lib/dynamic_programming.rb
@@ -2,8 +2,7 @@ class DynamicProgramming
 
   def initialize
     @blair_cache = {1 => 1, 2 => 2}
-    @frog_hops_bottom_up_cache = {}
-    @frog_cache_builder_cache = {}
+    # @frog_hops_bottom_up_cache = {}
     @frog_hops_top_down_cache = {}
     @super_from_hops_cache = {}
     @knapsack_cache = {}
@@ -11,6 +10,8 @@ class DynamicProgramming
 
   end
 
+#actually did this bottom up first, ask about this
+#is there any case where bottom up vs top down is better?
   def blair_nums(n)
     return @blair_cache[n] unless @blair_cache[n].nil?
 
@@ -18,15 +19,43 @@ class DynamicProgramming
       @blair_cache[el] = @blair_cache[el-1] + @blair_cache[el - 2] + (1 + 2*(el-2))
     end
 
-
     @blair_cache[n]
   end
 
+
   def frog_hops_bottom_up(n)
+    cache = frog_cache_builder(n)
+    cache[n]
 
   end
 
   def frog_cache_builder(n)
+    cache = {0 => [], 1 => [ [1] ], 2 => [ [1,1], [2] ], 3 => [[1,1,1], [1, 2], [2,1], [3]]}
+    #how to do without nested nested loops
+    (4..n).each do |el|
+
+      cache[el] = []
+
+      cache[el-3].each do |arr1|
+          new_el = [3] + arr1
+          cache[el] << new_el
+      end
+
+      cache[el-2].each do |arr2|
+          new_el = [2] + arr2
+          cache[el] << new_el
+      end
+
+      cache[el-1].each do |arr3|
+          new_el = [1] + arr3
+          cache[el] << new_el
+      end
+
+
+    end
+
+    cache
+
 
   end
 
