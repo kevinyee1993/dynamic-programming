0bc2df68ff850deb89935374af888234fab68bd6
Kevin Yee <kevinyee1993@gmail.com>
2018-05-13 15:53:22 -0700

    Come up with 2 solutions to is_unique that do not take up any extra space

--- hackerrank/is_unique.js
+++ hackerrank/is_unique.js
@@ -1,3 +1,6 @@
+//solving this problem using an additional data structure
+//solution is O(n) time because have to go through each char of the string
+//O(n) space because creating a new hash to keep track of the count
 function isUnique(str) {
   let charCounter = { };
 
@@ -16,8 +19,43 @@ function isUnique(str) {
   return true;
 }
 
+//solving this problem with no additional data structures
 
-console.log(isUnique("hello"));
-console.log(isUnique("helo"));
-console.log(isUnique("heo"));
-console.log(isUnique("hheo"));
+//first solution
+//O(n^2) time because there's a nested loop going through each char of the str
+//and comparing it with the rest of the chars in the str
+//O(1) space because not using any additional data structures
+
+//bottleneck is the nested loop, if had to optimize would work on that
+function isUnique2(str) {
+  for(let i = 0; i < str.length - 1; i++) {
+    for(let j = i + 1; j < str.length; j++) {
+      if(str[i] === str[j]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+//second solution
+//time: O(nlogn) because having to sort the string
+//space: O(1) because not creating any new data structure
+
+function isUnique3(str) {
+  let sortedStr = str.split("").sort().join("");
+
+  for(let i = 0; i < sortedStr.length - 1; i++) {
+    if(sortedStr[i] === sortedStr[i + 1]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isUnique3("hello"));
+console.log(isUnique3("heelo"));
+console.log(isUnique3("heo"));
+console.log(isUnique3("hhelo"));
