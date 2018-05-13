d547df20e15b9a5d2e6675a4dc8dd4f4c4bcd3b0
Kevin Yee <kevinyee1993@gmail.com>
2018-05-12 21:21:44 -0700

    Solve compare_triplets in ruby

--- hackerrank/compare_triplets.rb
+++ hackerrank/compare_triplets.rb
@@ -1,2 +1,19 @@
 def compareTriplets(a0, a1, a2, b0, b1, b2)
+  score1 = [a0, a1, a2]
+  score2 = [b0, b1, b2]
+
+  score_total_1 = 0
+  score_total_2 = 0
+
+  score1.each_index do |idx|
+    if score1[idx] > score2[idx]
+      score_total_1 += 1
+    elsif score2[idx] > score1[idx]
+      score_total_2 += 1
+    end
+  end
+
+  p [score_total_1, score_total_2]
 end
+
+compareTriplets(5, 6, 7, 3, 6, 10)
