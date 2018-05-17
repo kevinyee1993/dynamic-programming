b928ca6ce99aee95adb1fc92e16ee09710994af4
Kevin Yee <kevinyee1993@gmail.com>
2018-05-17 11:10:23 -0700

    Solve initial plates

---
+++ hackerrank/plates.js
@@ -0,0 +1,73 @@
+//array of stacks
+
+//keep track of the current stack
+
+//when that stack's length becomes too high, create a new stack in the
+//array and append the node onto that stack
+
+//when popping from the stacks, when get to a point where the current
+//stack becomes empty, switch current stacks to the previous one
+
+
+//time: push = O(1), pop = O(1)
+//because as the number of plates go up, so do the number of new plate stacks
+class StackPlates {
+  constructor(maxPlates) {
+    this.plateStore = [];
+    this.maxPlates = maxPlates;
+    this.currentStackIdx = null;
+  }
+
+  //push
+  push(plate) {
+    if(this.currentStackIdx === null) {
+      let newStack = [];
+      newStack.push(plate);
+      this.currentStackIdx = 0;
+      this.plateStore[this.currentStackIdx] = newStack;
+    } else if(this.plateStore[this.currentStackIdx].length < this.maxPlates) {
+      this.plateStore[this.currentStackIdx].push(plate);
+    } else {
+      let newStack = [];
+      newStack.push(plate);
+      this.currentStackIdx++;
+      this.plateStore[this.currentStackIdx] = newStack;
+    }
+  }
+
+  //pop
+  pop() {
+    if(this.plateStore[this.currentStackIdx].length > 0) {
+      this.plateStore[this.currentStackIdx].pop();
+    } else {
+      this.currentStackIdx--;
+      this.plateStore[this.currentStackIdx].pop();
+    }
+  }
+}
+
+let test = new StackPlates(3);
+test.push(1);
+test.push(1);
+test.push(1);
+test.push(1);
+test.push(1);
+test.push(1);
+test.push(1);
+test.push(1);
+console.log(test.plateStore);
+
+test.pop();
+test.pop();
+test.pop();
+
+test.push(1);
+test.push(1);
+test.pop();
+test.pop();
+test.pop();
+test.pop();
+test.pop();
+test.pop();
+test.pop();
+console.log(test.plateStore);
--- hackerrank/stack_min2.js
+++ hackerrank/stack_min2.js
@@ -1,13 +1,53 @@
+//different way of doing stack_min
+//correct way to do if you can't modify the node
+//just create a new stack that keeps track of the lowest values
+//and only updates when a new lowest value is appended
+
 class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
   }
 }
 
 class LinkedStack {
   constructor() {
     this.minStack = new MinStack();
+    this.head = null;
+    this.last = null;
+    this.prev = null;
+  }
+
+  append(node) {
+    if(!this.head) {
+      this.head = node;
+      this.last = node;
+    } else {
+      node.prev = this.last;
+      this.last.next = node;
+      this.last = node;
+    }
+
+    this.minStack.append(node);
+  }
+
+  pop() {
+    //pops from min stack if this node is the lowest value
+    if(this.last === this.minStack.min) {
+      this.minStack.pop();
+    }
+
+    if(this.last.prev) {
+      this.last.prev.next = null;
+      this.last = this.last.prev;
+    } else {
+      this.head = null;
+    }
+  }
+
+  min() {
+    return this.minStack.min;
   }
 
 
@@ -38,3 +78,24 @@ class MinStack {
     this.min = this.stack[this.stack.length - 1];
   }
 }
+
+let a = new LinkedStack();
+let node1 = new Node(1);
+let node2 = new Node(2);
+let node3 = new Node(3);
+let node4 = new Node(4);
+
+a.append(node4);
+a.append(node3);
+a.append(node2);
+a.append(node1);
+
+
+a.pop();
+console.log(a.min());
+a.pop();
+console.log(a.min());
+a.pop();
+console.log(a.min());
+a.pop();
+console.log(a.min());
