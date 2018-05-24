3442dfbcf86ac30998c350951c2cfede72536d9a
Kevin Yee <kevinyee1993@gmail.com>
2018-05-24 12:11:47 -0700

    Finish clear and update bit

--- hackerrank/bits.js
+++ hackerrank/bits.js
@@ -1,3 +1,33 @@
 function getBit(num, i) {
   return (num & (1 << i)) !== 0;
 }
+
+function setBit(num, i) {
+  let hasBit = ((num & (1 << i)) !== 0);
+  if(hasBit) {
+    num -= (1 << i);
+  } else {
+    num += (1 << i);
+  }
+
+  return num;
+}
+
+function clearBit(num, i) {
+  let mask = ~(1 << i);
+  return num & mask;
+}
+
+function updateBit(num, i, v) {
+  let mask = ~(1 << i);
+  num = num & mask;
+  return num | (v << i);
+}
+//1011 = 11
+//mask = 011
+//updateVal = 100
+//mask = 111
+//1011 111 = 1111
+
+//1111 = 15
+console.log(updateBit(11, 2, 1));
