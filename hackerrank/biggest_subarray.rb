7def1e0fa7aa85179c4420efd90c370f1eaa36c0
Kevin Yee <kevinyee1993@gmail.com>
2018-06-02 18:52:54 -0700

    Start biggest subarray

---
+++ hackerrank/biggest_subarray.rb
@@ -0,0 +1,4 @@
+# keep adding to subarray until find negative number
+# decide whether to keep going using that negative number or to stop
+# highest_total, curr_total, biggest_arr, curr_arr variables to keep
+# track of everything seems like
