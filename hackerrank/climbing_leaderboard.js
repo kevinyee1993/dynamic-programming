d4b84c40a77bb2d8d344b4fe6da092042e7d435a
Kevin Yee <kevinyee1993@gmail.com>
2018-06-04 20:29:23 -0700

    Create second solution but still needs work

--- hackerrank/climbing_leaderboard.js
+++ hackerrank/climbing_leaderboard.js
@@ -3,7 +3,7 @@
 // space complexity: O(n)
 // need to fix bottleneck of nested loop
 
-function climbingLeaderboard(scores, alice) {
+function climbingLeaderboard2(scores, alice) {
     let rankTrack = {};
     let currRank = 1;
     let order = [];
@@ -38,4 +38,41 @@ function climbingLeaderboard(scores, alice) {
     }
 
     return order;
+}
+
+function climbingLeaderboard(scores, alice) {
+    let combinedScores = [];
+
+    scores.forEach(score => {
+        combinedScores.push(parseInt(score));
+    })
+
+    alice.forEach(score => {
+        combinedScores.push(parseInt(score));
+    });
+
+
+    // O(n log(n)) here because of sorting
+    // combinedScores = combinedScores.sort().reverse();
+    combinedScores = combinedScores.sort((a, b) => a - b).reverse();
+
+
+
+    let rankTrack = {};
+    let currRank = 1;
+
+    combinedScores.forEach(score => {
+       if(!rankTrack[score]) {
+           rankTrack[score] = currRank;
+           currRank++;
+       }
+    });
+
+    console.log(rankTrack);
+
+    alice.forEach(score => {
+        console.log(rankTrack[score]);
+    });
+
+
 }
