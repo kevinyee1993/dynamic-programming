dbab183fc1e71e440ad7a5eea363740150e2122b
Kevin Yee <kevinyee1993@gmail.com>
2018-06-11 01:07:41 -0700

    More error practice

--- leetcode/try_catch_practice.js
+++ leetcode/try_catch_practice.js
@@ -1,9 +1,16 @@
+class FakeError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "FAKEError";
+  }
+}
+
 function test(input) {
   try {
     if(input) {
       console.log(input);
     } else {
-      throw new Error("No input submitted!!!?!?!?!?!?!??!?!!!!?!?");
+      throw new FakeError("No input submitted!!!?!?!?!?!?!??!?!!!!?!?");
     }
   } catch(e) {
     console.log(e);
