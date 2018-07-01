423e75018dd76f579e4c01df0f8a484caee039a7
Kevin Yee <kevinyee1993@gmail.com>
2018-06-30 19:29:36 -0700

    Lay out initial approach

--- leetcode/spiral_matrix2.rb
+++ leetcode/spiral_matrix2.rb
@@ -1,4 +1,21 @@
 def generate_matrix(n)
   # first line aka first array is always 1..n
-  
+
 end
+
+# start by initializing an N x N 2d array filled with undefineds
+
+# have a function called change directions which takes 2 params
+# change_directions(curr_direction, count)
+# there will be 4 directions: left, down, right, up
+# depending on the current direction, the direction switches to the next one
+# in the sequence
+# the count starts at one and keeps incrementing and being placed in
+
+# if reach the boundary of the 2d array or hit a number, change direction
+# and proceed adding numbers
+
+# stop once you place n^2 into the value
+
+# this approach will be O(n^2) time and space because have to create
+# the 2d array as well as place each element in given a starting value of n
