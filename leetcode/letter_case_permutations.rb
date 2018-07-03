90d2a2846bfc473593737255607abd0d21ddf78a
Kevin Yee <kevinyee1993@gmail.com>
2018-07-03 02:12:23 -0700

    Finish letter case permutation

--- leetcode/letter_case_permutations.rb
+++ leetcode/letter_case_permutations.rb
@@ -1,11 +1,33 @@
 def letter_case_permutation(s)
+  lowercase_str = s.downcase
 
-end
+  permutations = [lowercase_str]
 
-def convert_lowercase(s)
-  for i in 0...s.length
-    s[i] = s[i].upcase
+  letter_indices = find_letters(lowercase_str)
+
+  for i in 0...letter_indices.length
+    temp_str = lowercase_str.dup
+
+    for j in i...letter_indices.length
+      temp_str[letter_indices[j]] = temp_str[letter_indices[j]].upcase
+      permutations << temp_str.dup
+    end
+
   end
 
-  s
+  permutations
+
 end
+
+def find_letters(s)
+  letter_indices = []
+
+  for i in 0...s.length
+    letter_indices << i if /[a-zA-Z]/.match(s[i])
+  end
+
+  letter_indices
+end
+
+
+p letter_case_permutation("a1b2")
