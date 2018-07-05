9d0e506b506abe42bc6df501bb50e617b1e76ac9
Kevin Yee <kevinyee1993@gmail.com>
2018-07-05 12:53:29 -0700

    Finish move zeroes

--- leetcode/javaStuff/moveZeroes/moveZeroes.java
+++ leetcode/javaStuff/moveZeroes/moveZeroes.java
@@ -1,18 +1,29 @@
+import java.util.Arrays;
+
 class moveZeroes {
   public static void main(String args[]) {
-    int fart[] = {1,2,3};
-    // moveZeroes(new int[] {0,1,0,3,12});
-    swap(fart, 0, 1);
+    int fart[] = {0,1,0,3,1,12,0,0,4,2,1,0};
+    moveZeroes(fart);
+    // swap(fart, 0, 1);
 
-    for(int i = 0; i < fart.length; i++) {
-      System.out.println(fart[i]);
-    }
+    System.out.println(Arrays.toString(fart));
   }
 
 
   public static void moveZeroes(int[] nums) {
     int slowPointer = 0;
     int fastPointer = 1;
+
+    while(fastPointer < nums.length) {
+      if(nums[slowPointer] == 0 && nums[fastPointer] != 0) {
+        swap(nums, slowPointer, fastPointer);
+        slowPointer++;
+      } else if(nums[slowPointer] != 0) {
+        slowPointer++;
+      }
+
+      fastPointer++;
+    }
   }
 
   public static void swap(int[] nums, int sPoint, int fPoint) {
