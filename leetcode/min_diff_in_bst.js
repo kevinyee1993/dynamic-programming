9f9ef4722f9fff28d39d3db6af12c1bd75409b0c
Kevin Yee <kevinyee1993@gmail.com>
2018-07-06 20:58:45 -0700

    Start min diff in bst

---
+++ leetcode/min_diff_in_bst.js
@@ -0,0 +1,14 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function minDiffInBST(rootNode) {
+
+}
+
+let a = new TreeNode(4);
+console.log(a);
