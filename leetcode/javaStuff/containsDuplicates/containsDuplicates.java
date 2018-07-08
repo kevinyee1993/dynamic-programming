087380f5dfcb2e72f9b9acc589737e5b6dca664f
Kevin Yee <kevinyee1993@gmail.com>
2018-07-07 19:53:38 -0700

    Complete contains duplicates using java

---
+++ leetcode/javaStuff/containsDuplicates/containsDuplicates.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class containsDuplicates {
+  public static void main(String args[]) {
+    int[] a = {1,2,3,4,5,1};
+    System.out.println(containsDuplicates(a));
+  }
+
+  public static boolean containsDuplicates(int[] nums) {
+    // hash set similar to just regular hash
+    Set<Integer> set = new HashSet<>(nums.length);
+
+    // enhanced for loop -> check bucky
+    for(int x: nums) {
+      if(set.contains(x)) return true;
+      set.add(x);
+    }
+
+    return false;
+  }
+}
