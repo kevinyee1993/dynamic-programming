cdd2366826453db70d32a66222b9af11114c189e
Kevin Yee <kevinyee1993@gmail.com>
2018-07-12 14:51:02 -0700

    Finish lowercase with java

---
+++ leetcode/javaStuff/lowercase/Lowercase.java
@@ -0,0 +1,9 @@
+public class Lowercase {
+  public static void main (String[] args) {
+    System.out.println(lowercase("HEADFDS"));
+  }
+
+  public static String lowercase(String str) {
+    return str.toLowerCase();
+  }
+}
