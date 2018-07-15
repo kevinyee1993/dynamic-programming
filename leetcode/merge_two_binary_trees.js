e5f45dde9b41e9614b8bd43526e702fd6290e1ba
Kevin Yee <kevinyee1993@gmail.com>
2018-07-15 00:28:04 -0700

    Finish merge two binary trees

--- leetcode/merge_two_binary_trees.js
+++ leetcode/merge_two_binary_trees.js
@@ -1,40 +1,24 @@
+// time and space complexity of recursive function?
+
 function TreeNode(val) {
    this.val = val;
    this.left = this.right = null;
  }
 
 var mergeTrees = function(t1, t2) {
-  let t1Val;
-  let t2Val;
-  let t1Left;
-  let t1Right;
-  let t2Left;
-  let t2Right;
+  if(t1 === null && t2 === null) {
 
-  if(t1 === null) {
-    t1Val = null;
-     t1Left = null;
-     t1Right = null;
-  } else {
-    t1Val = t1.val;
-    t1Left = t1.left;
-    t1Right = t1.right;
+    return null;
+  } else if(t1 === null) {
+    return t2;
+  } else if(t2 === null) {
+    return t1;
   }
-  if(t2 === null) {
-    t2Val = null;
-     t2Left = null;
-     t2Right = null;
-  } else {
-  t2Val = t2.val;
-  t2Left = t2.left;
-  t2Right = t2.right;
-}
 
-  let combinedVal = t1Val + t2Val;
+  let combinedVal = t1.val + t2.val;
   let newNode = new TreeNode(combinedVal);
-
-  newNode.left = mergeTrees(t1Left + t2Left);
-  newNode.right = mergeTrees(t1Right + t2Right);
+  newNode.left = mergeTrees(t1.left, t2.left);
+  newNode.right = mergeTrees(t1.right, t2.right);
 
   return newNode;
 };
