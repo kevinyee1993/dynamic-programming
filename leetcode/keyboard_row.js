62a279e5d1a33b37b0ffc8bf9baa887c570e5675
Kevin Yee <kevinyee1993@gmail.com>
2018-07-17 08:19:40 -0700

    Make progress on keyboard rows and fix code smells

--- leetcode/keyboard_row.js
+++ leetcode/keyboard_row.js
@@ -1,12 +1,23 @@
+// time complexity: O(n) where n is the amount of characters total of the words
+// rather than the number of entries
+
 var findWords = function(words) {
   const row1 = createKeyboardRowHash('qwertyuiop',1);
   const row2 = createKeyboardRowHash('asdfghjkl',2);
   const row3 = createKeyboardRowHash('zxcvbnm',3);
 
   const combinedRowsHash = Object.assign(row1, row2, row3);
+  let answers = [];
+
+  // for(let i = 0; i < words; i++) {
+  //
+  // }
+
   console.log(combinedRowsHash);
 };
 
+
+// O(n) time complexity
 function createKeyboardRowHash(letters, rowNumber) {
   let keyboardRow = {};
 
@@ -16,5 +27,26 @@ function createKeyboardRowHash(letters, rowNumber) {
 
   return keyboardRow;
 }
+
+// function checkWord(word, combinedRowsHash) {
+//   let previousRow = undefined;
+//   for(let i = 0; i < word.length; i++) {
+//
+//     let currentRow = combinedRowsHash[word[i]];
+//
+//     if(previousRow) {
+//       if(previousRow !== currentRow) {
+//         return false;
+//       }
+//
+//       previousRow = currentRow;
+//
+//     } else {
+//       previousRow = currentRow;
+//     }
+//   }
+//
+//   return true;
+// }
 
 findWords();
