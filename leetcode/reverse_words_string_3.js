f1590061ae9ff606884a3a3e5db25312177b4ffc
Kevin Yee <kevinyee1993@gmail.com>
2018-07-17 07:49:48 -0700

    Finish reverse string 3

--- leetcode/reverse_words_string_3.js
+++ leetcode/reverse_words_string_3.js
@@ -3,9 +3,25 @@
 var reverseWords = function(s) {
   let newStr = "";
 
+  // O(n) operation
   for(let i = 0; i < s.length; i++) {
     newStr += s[s.length - 1 - i];
   }
 
-  return newStr;
+  let stringToArray = newStr.split(" ");
+
+  // O(n) operation
+  flipArray(stringToArray);
+
+  return stringToArray.join(" ");
 };
+
+function flipArray(arr) {
+  for(let i = 0; i < arr.length / 2; i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length - 1 - i];
+    arr[arr.length - 1 - i] = temp;
+  }
+}
+
+console.log(reverseWords("farts parts barts"));
