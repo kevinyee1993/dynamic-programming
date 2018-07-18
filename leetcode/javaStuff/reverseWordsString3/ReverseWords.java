0319fc1cb913a8a8a9db7025b52a4a5cfbe7fb22
Kevin Yee <kevinyee1993@gmail.com>
2018-07-18 07:57:36 -0700

    Refactor code to use split string into array first

--- leetcode/javaStuff/reverseWordsString3/ReverseWords.java
+++ leetcode/javaStuff/reverseWordsString3/ReverseWords.java
@@ -1,14 +1,19 @@
 public class ReverseWords {
   public static void main(String[] args) {
     String butt = "butt";
-    String[] words = {"fart", "part", "bart"};
+    // String[] words = {"fart", "part", "bart"};
+    String words = "fart part bart";
+    String[] wordsArr = new String[words.length()];
+    wordsArr = words.split(" ");
 
     // System.out.println(reverseSingleWord(butt));
     // System.out.println(reverseWords(words));
 
-    words = reverseWords(words);
-    for(int i = 0; i < words.length; i++) {
-      System.out.println(words[i]);
+    // words = reverseWords(wordsArr);
+    reverseWords(wordsArr);
+
+    for(int i = 0; i < wordsArr.length; i++) {
+      System.out.println(wordsArr[i]);
     }
   }
 
