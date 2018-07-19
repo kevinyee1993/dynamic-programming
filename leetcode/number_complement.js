3a6f17ca0786cc9bbb3afcc80e8479680cc75bad
Kevin Yee <kevinyee1993@gmail.com>
2018-07-19 08:51:09 -0700

    Update number complement

--- leetcode/number_complement.js
+++ leetcode/number_complement.js
@@ -1,3 +1,35 @@
 var findComplement = function(num) {
 
 };
+
+
+// don't start at 1, should start at max if you wanna take this approach
+function turnNumberToBit(num) {
+  let bit = "";
+  let currentBit = 1;
+
+  if(num === 0) {
+    return "0";
+  }
+
+  while(num > 0) {
+    console.log(num);
+    if(num >= currentBit) {
+      num -= currentBit;
+      bit = "1" + bit;
+    } else {
+      bit = "0" + bit;
+    }
+
+    currentBit *= 2;
+  }
+
+  return bit;
+}
+
+function turnBitToNumber(bit) {
+
+}
+
+console.log(turnNumberToBit(5));
+// console.log(turnNumberToBit(2));
