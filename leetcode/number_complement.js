98dcc37f33006216cca66180537db9230c4885ff
Kevin Yee <kevinyee1993@gmail.com>
2018-07-20 15:56:29 -0700

    Change approach to problem

--- leetcode/number_complement.js
+++ leetcode/number_complement.js
@@ -1,35 +1,46 @@
 var findComplement = function(num) {
 
+  // turn num to its complement
+  let numToComplement = ~num;
+
+
+  // turn leftmost 1 into a 0
+  // leading 0 with all 1s | with the other numbers should give the non negative complement
+
+
+  
 };
 
 
-// don't start at 1, should start at max if you wanna take this approach
-function turnNumberToBit(num) {
-  let bit = "";
-  let currentBit = 1;
 
-  if(num === 0) {
-    return "0";
-  }
 
-  while(num > 0) {
-    console.log(num);
-    if(num >= currentBit) {
-      num -= currentBit;
-      bit = "1" + bit;
-    } else {
-      bit = "0" + bit;
-    }
+// oh can just take a number complement with ~num and then turn the
+// very biggest bit (which turned into a 1) into a 0.
 
-    currentBit *= 2;
-  }
-
-  return bit;
-}
-
-function turnBitToNumber(bit) {
-
-}
-
-console.log(turnNumberToBit(5));
-// console.log(turnNumberToBit(2));
+// // don't start at 1, should start at max if you wanna take this approach
+// function turnNumberToBit(num) {
+//   let bit = "";
+//   let currentBit = 1;
+//
+//   if(num === 0) {
+//     return "0";
+//   }
+//
+//   while(num > 0) {
+//     console.log(num);
+//     if(num >= currentBit) {
+//       num -= currentBit;
+//       bit = "1" + bit;
+//     } else {
+//       bit = "0" + bit;
+//     }
+//
+//     currentBit *= 2;
+//   }
+//
+//   return bit;
+// }
+//
+// function turnBitToNumber(bit) {
+//
+// }
