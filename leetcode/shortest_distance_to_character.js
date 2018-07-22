043043eb3d4c104b97a7b9d50f4b9c5e031ae668
Kevin Yee <kevinyee1993@gmail.com>
2018-07-21 18:36:54 -0700

    Create pseudocode for shortest ditsance to character

--- leetcode/shortest_distance_to_character.js
+++ leetcode/shortest_distance_to_character.js
@@ -7,8 +7,38 @@ var shortestToChar = function(S, C) {
   // create a method to find the next C's position
   let nextC = null;
 
+  // in java you would make this array as long as however long the string is
+  let answer = [];
+
   // You want use whichever value previousC || nextC which is lower
   // and then add that to the array
   // can just use previousC but for nextC you wanna use nextC - currentIndex
-  
+
+
+  // loop through the string.  if previousC is null or 0, call findNextC
+  for(let i = 0; i < S.length; i++) {
+    if(nextC === null || previousC === 0) {
+      findNextC(i, S, C);
+    }
+
+    if(S[i] === C) {
+      previousC = 0;
+    } else if(previousC !== null) {
+      previousC++;
+    }
+
+    if(previousC !== null && previousC < nextC - i) {
+      answer.push(previousC);
+    } else {
+      answer.push(nextC - i);
+    }
+  }
+
+  return answer;
 };
+
+
+//TODO
+function findNextC(currentIndex, S, C) {
+
+}
