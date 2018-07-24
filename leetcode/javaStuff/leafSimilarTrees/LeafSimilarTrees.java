d2ce3b173773a880684a7d1be09f032f1ff17fb0
Kevin Yee <kevinyee1993@gmail.com>
2018-07-24 14:35:23 -0700

    Start leaf similar trees

---
+++ leetcode/javaStuff/leafSimilarTrees/LeafSimilarTrees.java
@@ -0,0 +1,15 @@
+public class LeafSimilarTrees {
+  public static void main(String[] args) {
+
+  }
+
+
+  public class TreeNode {
+   int val;
+   TreeNode left;
+   TreeNode right;
+   TreeNode(int x) { val = x; }
+ }
+
+
+}
