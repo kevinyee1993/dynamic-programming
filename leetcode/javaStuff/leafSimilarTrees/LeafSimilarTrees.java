ec459d79af4b5339f81accbb7d1da5e107c74e74
Kevin Yee <kevinyee1993@gmail.com>
2018-07-24 16:04:17 -0700

    Create method to get leaf node sequence when given a head node

--- leetcode/javaStuff/leafSimilarTrees/LeafSimilarTrees.java
+++ leetcode/javaStuff/leafSimilarTrees/LeafSimilarTrees.java
@@ -2,13 +2,21 @@ import java.util.*;
 
 public class LeafSimilarTrees {
   public static void main(String[] args) {
-    ArrayList<Integer> a = new ArrayList<Integer>();
-    ArrayList<Integer> b = new ArrayList<Integer>();
+    // ArrayList<Integer> a = new ArrayList<Integer>();
+    // ArrayList<Integer> b = new ArrayList<Integer>();
 
-    a.add(1);
-    b.add(1);
+    // a.add(1);
+    // b.add(1);
 
-    System.out.println(compareLeafSequences(a, b));
+    // way to combine ArrayLists
+    // a.addAll(b);
+    TreeNode a = new TreeNode(1);
+    a.left = new TreeNode(2);
+    a.right = new TreeNode(3);
+    a.right.right = new TreeNode(4);
+
+    System.out.println(getLeafSequence(a));
+    // System.out.println(compareLeafSequences(a, b));
 
   }
   //
@@ -34,8 +42,33 @@ public class LeafSimilarTrees {
     return true;
   }
 
+  public static ArrayList<Integer> getLeafSequence(TreeNode head) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
 
-  public class TreeNode {
+    ArrayList<Integer> leftList = new ArrayList<Integer>();
+    ArrayList<Integer> rightList = new ArrayList<Integer>();
+
+    // base case
+    if(head.left == null && head.right == null) {
+      list.add(head.val);
+      return list;
+    }
+
+    if(head.left != null) {
+      leftList = getLeafSequence(head.left);
+    }
+
+    if(head.right != null) {
+      rightList = getLeafSequence(head.right);
+    }
+
+    leftList.addAll(rightList);
+
+    return leftList;
+  }
+
+
+  public static class TreeNode {
    int val;
    TreeNode left;
    TreeNode right;
