03e07f7f7a5f71d02337b0e0bbbd18b6a5a67ae9
Kevin Yee <kevinyee1993@gmail.com>
2018-07-25 15:59:28 -0700

    Finish return kthToLast

--- leetcode/javaStuff/CTCI_linkedLists/returnKthToLast/ReturnKthToLast.java
+++ leetcode/javaStuff/CTCI_linkedLists/returnKthToLast/ReturnKthToLast.java
@@ -4,22 +4,29 @@ public class ReturnKthToLast {
   public static void main(String[] args) {
     ListNode test = new ListNode(1);
     ListNode test2 = new ListNode(2);
+    ListNode test3 = new ListNode(3);
+    ListNode test4 = new ListNode(4);
+    ListNode test5 = new ListNode(5);
 
     test.setNext(test2);
+    test2.setNext(test3);
+    test3.setNext(test4);
+    test4.setNext(test5);
 
-    ArrayList<ListNode> butt = createListFromHead(test);
-
-    for(int i = 0; i < butt.size(); i++) {
-      System.out.println(butt.get(i).value);
-    }
+    System.out.println(returnKthToLast(4, test).value);
   }
 
-  // public static ListNode returnKthToLast(int n, ListNode head) {
-  //   ArrayList<ListNode> allNodes = createListFromHead(head);
-  //   // 0th from last is last one
-  //   // 1st to last is second to last etc.
-  // }
+  public static ListNode returnKthToLast(int n, ListNode head) {
+    ArrayList<ListNode> allNodes = createListFromHead(head);
 
+    // just O(1) lookup time
+    int targetNode = allNodes.size() - 1 - n;
+
+    return allNodes.get(targetNode);
+  }
+
+
+  // O(n) time and space
   public static ArrayList<ListNode> createListFromHead(ListNode head) {
     ArrayList<ListNode> nodeList = new ArrayList<ListNode>();
     ListNode currentNode = head;
