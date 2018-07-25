95cb3ce5301064e4a34e8663c859f7527707665c
Kevin Yee <kevinyee1993@gmail.com>
2018-07-25 15:56:32 -0700

    Create method to create array list of nodes from head node

--- leetcode/javaStuff/CTCI_linkedLists/returnKthToLast/ReturnKthToLast.java
+++ leetcode/javaStuff/CTCI_linkedLists/returnKthToLast/ReturnKthToLast.java
@@ -1,10 +1,34 @@
+import java.util.*;
+
 public class ReturnKthToLast {
   public static void main(String[] args) {
     ListNode test = new ListNode(1);
     ListNode test2 = new ListNode(2);
 
     test.setNext(test2);
-    System.out.println(test.value);
-    System.out.println(test.getNext().value);
+
+    ArrayList<ListNode> butt = createListFromHead(test);
+
+    for(int i = 0; i < butt.size(); i++) {
+      System.out.println(butt.get(i).value);
+    }
+  }
+
+  // public static ListNode returnKthToLast(int n, ListNode head) {
+  //   ArrayList<ListNode> allNodes = createListFromHead(head);
+  //   // 0th from last is last one
+  //   // 1st to last is second to last etc.
+  // }
+
+  public static ArrayList<ListNode> createListFromHead(ListNode head) {
+    ArrayList<ListNode> nodeList = new ArrayList<ListNode>();
+    ListNode currentNode = head;
+
+    while(currentNode != null) {
+      nodeList.add(currentNode);
+      currentNode = currentNode.getNext();
+    }
+
+    return nodeList;
   }
 }
