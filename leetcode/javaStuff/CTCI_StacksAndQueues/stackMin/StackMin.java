2c7d707bec7bb1d292c4573f68d1aee6741855c2
Kevin Yee <kevinyee1993@gmail.com>
2018-07-26 11:06:23 -0700

    Test out generic types

---
+++ leetcode/javaStuff/CTCI_StacksAndQueues/stackMin/StackMin.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class StackMin {
+
+  public static void main(String[] args) {
+    test<Integer> farts = new test<Integer>(3);
+
+    System.out.println(farts.get());
+
+  }
+}
---
+++ leetcode/javaStuff/CTCI_StacksAndQueues/stackMin/test.java
@@ -0,0 +1,12 @@
+public class test<T> {
+
+  private T value;
+
+  public test(T value) {
+    this.value = value;
+  }
+
+  public T get() {
+    return this.value;
+  }
+}
