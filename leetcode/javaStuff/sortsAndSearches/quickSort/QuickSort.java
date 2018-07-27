4227419c1d5dee32f6ac6a3b3ab6840006cd2764
Kevin Yee <kevinyee1993@gmail.com>
2018-07-27 15:44:15 -0700

    Set up pseudocode for overloaded quickosrt methods

--- leetcode/javaStuff/sortsAndSearches/quickSort/QuickSort.java
+++ leetcode/javaStuff/sortsAndSearches/quickSort/QuickSort.java
@@ -3,7 +3,27 @@ public class QuickSort {
 
   }
 
-  public static int[] quickSort(int[] arr) {
-    
+  public static void quickSort(int[] arr) {
+    quickSort(arr, 0, arr.length - 1);
+  }
+
+  public static void quickSort(int[] arr, int left, int right) {
+    if(left >= right) {
+      return;
+    }
+
+    int pivot = arr[(left + right) / 2];
+    int index = partition(arr, left, right, pivot);
+    quickSort(arr, left, index - 1);
+    quickSort(arr, index, right);
+
+
+  }
+
+  // Does all the swapping and then returns the point which divides
+  // all the elements lower than the pivot and all the elements greater
+  // than the pivot
+  public static int partition(int[] arr, int left, int right, int pivot) {
+
   }
 }
