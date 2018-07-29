50525ab8dcc0d861b765d2812c5dd5b10ec4ea40
Kevin Yee <kevinyee1993@gmail.com>
2018-07-29 16:31:24 -0700

    Create ListNode class and start middle of linked list

---
+++ leetcode/javaStuff/middleOfLinkedList/ListNode.java
@@ -0,0 +1,9 @@
+public class ListNode {
+
+  int value;
+  int next;
+
+  public ListNode(int value) {
+    this.value = value;
+  }
+}
---
+++ leetcode/javaStuff/middleOfLinkedList/MiddleLinkedList.java
@@ -0,0 +1,5 @@
+public class MiddleLinkedList {
+  public static void main(String[] args) {
+
+  }
+}
