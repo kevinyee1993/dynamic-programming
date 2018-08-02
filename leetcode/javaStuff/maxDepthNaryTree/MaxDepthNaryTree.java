89746c5d7a61d58d87b83677d314654733e515b8
Kevin Yee <kevinyee1993@gmail.com>
2018-08-02 13:34:13 -0700

    Start max depth N-ary tree and create tree node class

---
+++ leetcode/javaStuff/maxDepthNaryTree/MaxDepthNaryTree.java
@@ -0,0 +1,9 @@
+public class MaxDepthNaryTree {
+  public static void main(String[] args) {
+
+  }
+
+  public static int maxDepthNaryTree(TreeNode root) {
+    
+  }
+}
---
+++ leetcode/javaStuff/maxDepthNaryTree/TreeNode.java
@@ -0,0 +1,10 @@
+public static TreeNode {
+  int value;
+  TreeNode left;
+  TreeNode right;
+  TreeNode middle;
+
+  public TreeNode(int value) {
+    this.value = value;
+  }
+}
