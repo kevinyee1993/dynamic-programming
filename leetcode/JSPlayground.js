4790c93878dd03d71e41f58664a68b50a93c3a95
Kevin Yee <kevinyee1993@gmail.com>
2018-08-03 14:00:48 -0700

    Just testing js scopes

--- leetcode/JSPlayground.js
+++ leetcode/JSPlayground.js
@@ -1,14 +1,31 @@
 class Person {
   constructor(name) {
     this.name = name;
+    // this.testFunction = this.testFunction.bind(this);
+
+
+    this.testFunction = () => {
+      console.log(this.name);
+    };
   }
+
 }
 
-let a = function(greeting, compliment) {
-  console.log(greeting + " " + this.name + compliment);
-};
+let person = new Person("kelving");
 
-let person1 = new Person("kevin");
-let args = ["hello", " you a snack brody"];
-// a.apply(person1, args);
-a.call(person1, "hello", " u da bes");
+let methurd = person.testFunction;
+
+methurd();
+
+
+
+
+
+// let a = function(greeting, compliment) {
+//   console.log(greeting + " " + this.name + compliment);
+// };
+//
+// let person1 = new Person("kevin");
+// let args = ["hello", " you a snack brody"];
+// // a.apply(person1, args);
+// a.call(person1, "hello", " u da bes");
