812f6f8ddad2f3717978025f67342ebcfc5203ba
Kevin Yee <kevinyee1993@gmail.com>
2018-08-04 18:12:04 -0700

    Start BST to greater tree conversion

---
+++ leetcode/javaStuff/convertBSTtoGreaterTree/BSTtoGreaterTree.java
@@ -0,0 +1,7 @@
+public class BSTtoGreaterTree {
+  public static void main(String[] args) {
+
+  }
+
+  
+}
---
+++ leetcode/javaStuff/convertBSTtoGreaterTree/TreeNode.java
@@ -0,0 +1,9 @@
+public class TreeNode {
+  int value;
+  TreeNode left;
+  TreeNode right;
+
+  public TreeNode(int value) {
+    this.value = value;
+  }
+}
