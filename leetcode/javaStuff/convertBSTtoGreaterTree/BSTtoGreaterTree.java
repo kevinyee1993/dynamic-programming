1db198092e4ba0ba8204c4cca7586e24d0a37825
Kevin Yee <kevinyee1993@gmail.com>
2018-08-04 18:47:55 -0700

    Make progress

--- leetcode/javaStuff/convertBSTtoGreaterTree/BSTtoGreaterTree.java
+++ leetcode/javaStuff/convertBSTtoGreaterTree/BSTtoGreaterTree.java
@@ -26,9 +26,35 @@ public class BSTtoGreaterTree {
   // will essentially be the sum of all greater values than
   // the node you're looking at
 
-  // public void convertBSTtoGreaterTree(TreeNode root) {
-  //
-  // }
+  // all left values add new root value
+  // all right values go through the same process
+
+  // wait left doesn't just add the top node but it also needs to
+  // add all of its rights
+
+  // what if first you convert all nodes normally using recursion
+  // then at the end go through the tree again and just add the
+  // root node value to each of its' lefts
+
+  public void convertBSTtoGreaterTree(TreeNode root) {
+    if(root == null) {
+      return;
+    }
+
+    // this is a stupid slow step actually
+    // can optimze this because you are doing a O(n) operation
+    // each stack frame but whatever man just get this done first
+    if(root.right != null) {
+      root.value += sumAllNodeValues(root.right);
+    }
+
+    // this is exponential step too because each stack frame is
+    // calling two more stack frames..!!!!
+    convertBSTtoGreaterTree(root.left);
+    convertBSTtoGreaterTree(root.right);
+
+
+  }
 
   // call this not on the root bc would make the recursion weird
   // but call it using the target node's right node as the param
