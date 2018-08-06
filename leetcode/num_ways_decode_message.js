bcdd00f794d36a979c28558993e3638abfa30479
Kevin Yee <kevinyee1993@gmail.com>
2018-08-06 15:16:43 -0700

    Create dictionary

--- leetcode/num_ways_decode_message.js
+++ leetcode/num_ways_decode_message.js
@@ -11,7 +11,51 @@ function numWaysDecode(message) {
 }
 
 function helper(message, numLastLetters) {
-  if(message[0] === '0' || message === "") {
+  let startRef = message.length - numLastLetters;
+  if(message[startRef] === '0') {
     return 0;
   }
+
+  if(message === "") {
+    return 1;
+  }
+
+  const numToLetters = {
+    "1": "a",
+    "2": "b",
+    "3": "c",
+    "4": "d",
+    "5": "e",
+    "6": "f",
+    "7": "g",
+    "8": "h",
+    "9": "i",
+    "10": "j",
+    "11": "k",
+    "12": "l",
+    "13": "m",
+    "14": "n",
+    "15": "o",
+    "16": "p",
+    "17": "q",
+    "18": "r",
+    "19": "s",
+    "20": "t",
+    "21": "u",
+    "22": "v",
+    "23": "w",
+    "24": "x",
+    "25": "y",
+    "26": "z"
+  };
+
+  if( !numToLetters[message[startRef] + message[startRef + 1]]) {
+    return 0;
+  }
+
+  let firstOptionCount = 1 + helper(message, numLastLetters - 1);
+  let secondOptionCount = 1 + helper(message, numLastLetters - 2);
+
+  return firstOptionCount + secondOptionCount;
+
 }
