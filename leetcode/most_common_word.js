5195f2b083575c67a2f3088feac8538bea935e5c
Kevin Yee <kevinyee1993@gmail.com>
2018-08-07 18:38:58 -0700

    Optimze time from O(nm) to O(n)

--- leetcode/most_common_word.js
+++ leetcode/most_common_word.js
@@ -2,25 +2,43 @@
 Naive solution
 time: O(nm) where n is the length of the paragraph and m is the length of banned words
 space: O(n) linear because creating new strArr and hash
+
+First optimization
+time: O(n) turned banned array into hash and just did constant time lookup
+for each word in that hash instead of looping through the entire array multiple times
 */
 
 function mostCommonWord(paragraph, banned) {
   paragraph = paragraph.replace(/[^a-zA-Z ]/g, '').toLowerCase();
 
   let strArr = paragraph.split(" ");
+  let bannedHash = {};
   let wordCounter = {};
   let biggestCount = 0;
   let mostWord = null;
 
-
   banned.forEach(word => {
-    for(let i = 0; i < strArr.length; i++) {
-      if(strArr[i] === word.toLowerCase()) {
-        strArr.splice(i, 1);
-        i--;
-      }
-    }
+    bannedHash[word.toLowerCase()] = true;
   });
+
+  for(let i = 0; i < strArr.length; i++) {
+    let word = strArr[i].toLowerCase();
+
+    if(bannedHash[word]) {
+      strArr.splice(i, 1);
+      i--;
+    }
+  }
+
+
+  // banned.forEach(word => {
+  //   for(let i = 0; i < strArr.length; i++) {
+  //     if(strArr[i] === word.toLowerCase()) {
+  //       strArr.splice(i, 1);
+  //       i--;
+  //     }
+  //   }
+  // });
 
   console.log(strArr);
 
