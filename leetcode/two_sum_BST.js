e9f67e900feb9a9910c0cbca0eec25a524e50353
Kevin Yee <kevinyee1993@gmail.com>
2018-08-08 17:32:39 -0700

    Finish two sum bst and account for edge case where root value == target value - root value

--- leetcode/two_sum_BST.js
+++ leetcode/two_sum_BST.js
@@ -7,23 +7,23 @@ class TreeNode {
 }
 
 let a = new TreeNode(6);
-let b = new TreeNode(4);
+let b = new TreeNode(3);
 let c = new TreeNode(8);
 
 a.left = b;
 a.right = c;
-
-let d = new TreeNode(3);
-let e = new TreeNode(5);
-
-b.left = d;
-b.right = e;
-
-let f = new TreeNode(7);
-let g = new TreeNode(9);
-
-c.left = f;
-c.right = g;
+//
+// let d = new TreeNode(3);
+// let e = new TreeNode(5);
+//
+// b.left = d;
+// b.right = e;
+//
+// let f = new TreeNode(7);
+// let g = new TreeNode(9);
+//
+// c.left = f;
+// c.right = g;
 
 
 
@@ -42,7 +42,6 @@ function binarySearch(root, target) {
     return false;
   }
 
-
   if(root.value > target && root.left) {
     return binarySearch(root.left, target);
   }
@@ -56,8 +55,36 @@ function binarySearch(root, target) {
   return false;
 }
 
-console.log(binarySearch(a, 3));
 
-// function twoSumBST(root) {
-//
-// }
+function findTarget(root, k) {
+  // O(n) space, creates a BST type of deal
+  let queue = [root];
+
+  while(queue.length > 0) {
+    let currentNode = queue[0];
+    if(currentNode.left) {
+      queue.push(currentNode.left);
+    }
+
+    if(currentNode.right) {
+      queue.push(currentNode.right);
+    }
+
+    let nodeToCheck = queue.shift();
+    let valueToFind = k - nodeToCheck.value;
+
+    if(root.value !== valueToFind) {
+      if(binarySearch(root, valueToFind)) {
+        return true;
+      }
+    } else if(nodeToCheck.left.value === valueToFind){
+      return true;
+    }
+
+  }
+
+  return false;
+}
+
+// console.log(binarySearch(a, 3));
+console.log(findTarget(a, 11));
