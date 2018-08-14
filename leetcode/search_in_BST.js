8c0ca1ed3a8200cd5dae180237f5378d8f13ccb9
Kevin Yee <kevinyee1993@gmail.com>
2018-08-13 17:06:16 -0700

    Finish search in BST

--- leetcode/search_in_BST.js
+++ leetcode/search_in_BST.js
@@ -9,10 +9,12 @@ class TreeNode {
 // this is implementing DFS if using recursion bc of stacks
 // BFS uses queue and no recursion typically
 function searchBST(root, val) {
+  if(root === null) {
+    return null;
+  }
+
   if(root.value === val) {
     return root;
-  } if(root === null) {
-    return null;
   }
 
   let left = searchBST(root.left, val);
@@ -26,3 +28,22 @@ function searchBST(root, val) {
     return right;
   }
 }
+
+let a = new TreeNode(5);
+
+let b = new TreeNode(3);
+let c = new TreeNode(2);
+let d = new TreeNode(1);
+
+let e = new TreeNode(7);
+let f = new TreeNode(6);
+let g = new TreeNode(8);
+
+a.left = b;
+a.right = e;
+b.left = c;
+b.right = d;
+e.left = f;
+e.right = g;
+
+console.log(searchBST(a, 10));
