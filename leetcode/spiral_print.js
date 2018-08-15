aed035bd7da50d3039d6129bf3e4be2f8cb2a853
Kevin Yee <kevinyee1993@gmail.com>
2018-08-15 16:08:17 -0700

    Progress on spiral print

--- leetcode/spiral_print.js
+++ leetcode/spiral_print.js
@@ -7,14 +7,62 @@ function spiralPrint(matrix) {
   let currX = 0;
   let currY = 0;
 
+  let currDirection = "right";
+
   let numElements = matrix.length * matrix[0].length;
 
   console.log(numElements);
 
   // loops through every single element
-  // for(let i = 0; i < numElements; i++) {
+  for(let i = 0; i < numElements; i++) {
+    switch(currDirection) {
+      case "right":
+        console.log(matrix[currY][currX]);
+        currX++;
 
-  // }
+        if(currX >= rightLimit) {
+          currDirection = "down";
+          rightLimit--;
+        }
+        break;
+      case "down":
+        console.log(matrix[currY][currX]);
+        currY++;
+
+        if(currY >= downLimit) {
+          currDirection = "left";
+          downLimit--;
+        }
+        break;
+      case "left":
+        console.log(matrix[currY][currX]);
+        currX--;
+
+        if(currX <= leftLimit) {
+          currDirection = "up";
+          leftLimit++;
+        }
+        break;
+      case "up":
+      console.log(matrix[currY][currX]);
+      currY--;
+
+      if(currX <= upLimit) {
+        currDirection = "right";
+        leftLimit++;
+      }
+      break;
+    }
+  }
+}
+
+function changeDirection(dir) {
+  switch(dir) {
+    case 'right': return "down";
+    case 'down': return "left";
+    case 'left': return "up";
+    case 'up': return "right";
+  }
 }
 
 let matrix = [[1,2,3],[4,5,6],[1,2,3],[1,2,3]];
