18a5e4132d7580658bc99de83b115d2d317aa073
Kevin Yee <kevinyee1993@gmail.com>
2018-08-14 20:26:34 -0700

    Finish group anagram

--- leetcode/group_anagrams.js
+++ leetcode/group_anagrams.js
@@ -1,6 +1,7 @@
 function groupAnagrams(strs) {
   let anagramHash = {};
   let strArr;
+  let answer = [];
 
   strs.forEach(str => {
     strArr = str.split("").sort();
@@ -11,54 +12,14 @@ function groupAnagrams(strs) {
     }
   });
 
-  console.log(anagramHash);
+  // extract the keys later brody
+  let hashKeys = Object.keys(anagramHash);
+  hashKeys.forEach(key => {
+    answer.push(anagramHash[key]);
+  });
+
+  return answer;
 }
 
 
-// O(n) space and O(nlog(n)) time
-// function isAnagram(str1, str2) {
-//   if(!str1 || !str2 || str1.length !== str2.length) {
-//     return false;
-//   }
-//
-//   let arr1 = str1.split("").sort();
-//   let arr2 = str2.split("").sort();
-//
-//   for(let i = 0; i < arr1.length; i++) {
-//     if(arr1[i] !== arr2[i]) {
-//       return false;
-//     }
-//   }
-//
-//   return true;
-// }
-
-
 console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
-
-
-// just leaving this here for now
-// let answer = [];
-// let checkedWord;
-//
-// while(strs.length > 0) {
-//   checkedWord = strs.shift();
-//   let counter = 0;
-//   let subArr = [checkedWord];
-//
-//   let currentWord = strs[0];
-//
-//   while(currentWord) {
-//     if(isAnagram(checkedWord, currentWord)) {
-//       subArr.push(currentWord);
-//       strs.splice(counter, 1);
-//     } else {
-//       counter++;
-//       currentWord = strs[counter];
-//     }
-//   }
-//
-//   answer.push(subArr);
-// }
-//
-// return answer;
