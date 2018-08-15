1557efa758625e7bb275377fc4e56587110033bb
Kevin Yee <kevinyee1993@gmail.com>
2018-08-15 10:51:59 -0700

    Implement binary search

--- leetcode/search_in_rotated_array.js
+++ leetcode/search_in_rotated_array.js
@@ -1,8 +1,19 @@
+// O(n) time and O(n) space
 function searchRotatedArray(arr, target) {
+  // O(n) time
+  let slicePoint = findElementDrop(arr);
+
+  // O(n) space
+  let big = arr.slice(0, slicePoint);
+  let small = arr.slice(slicePoint, arr.length);
+  let newSorted = small.concat(big);
+
+  // O(log(n)) time
+  return binarySearch(newSorted, target);
 
 }
 
-// O(n) time
+// O(n) time, O(1) space
 function findElementDrop(arr) {
 
   for(let i = 0; i < arr.length; i++) {
@@ -15,5 +26,35 @@ function findElementDrop(arr) {
   return 0;
 }
 
-let test = [5,7,9,1,3,4];
-console.log(findElementDrop(test));
+
+// remember this should return the index
+// doing this the space intensive way but whatevs
+function binarySearch(arr, target) {
+  if(arr.length === 0) {
+    return -1;
+  } else if(arr.length === 1 && arr[0] === target) {
+    return 0;
+  } else if(arr.length === 1 && arr[0] !== target) {
+    return -1;
+  }
+
+  let midPoint = Math.floor(arr.length / 2);
+
+  if(arr[midPoint] === target) {
+    return midPoint;
+  } else if(arr[midPoint] > target) {
+    return binarySearch(arr.slice(0, midPoint), target);
+  } else if(arr[midPoint] < target) {
+    let store = binarySearch(arr.slice(midPoint, arr.length), target);
+    if(store === -1) {
+      return -1;
+    } else {
+      return midPoint + store;
+    }
+  }
+}
+
+// let test = [5,7,9,1,3,4];
+// let sortedBorted = [1,3,10,11];
+// console.log(binarySearch(sortedBorted, 11));
+// console.log(findElementDrop(test));
