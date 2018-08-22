0711823f16cc9d046504c8695c701d4e0e82d038
Kevin Yee <kevinyee1993@gmail.com>
2018-08-21 21:50:34 -0700

    Do heapify up

--- leetcode/treesAndGraphs/minHeap.js
+++ leetcode/treesAndGraphs/minHeap.js
@@ -5,7 +5,7 @@ class minHeap {
 
   insertElement(el) {
     this.heap.push(el);
-    this.heapifyUp(el);
+    this.heapifyUp();
   }
 
   removeMinimumElement() {
@@ -16,11 +16,33 @@ class minHeap {
     return this.heap[0];
   }
 
-  heapifyUp(el) {
-    
+  heapifyUp() {
+    let newestIdx = this.heap.length - 1;
+    // let newestEl = this.heap[lastIdx];
+    let parentIdx = this.getParentFromIndex(newestIdx);
+
+    // let parentEl = this.heap[parentIdx];
+
+    while(this.heap[newestIdx] < this.heap[parentIdx]) {
+      let temp = this.heap[newestIdx];
+      this.heap[newestIdx] = this.heap[parentIdx];
+      this.heap[parentIdx] = temp;
+
+      newestIdx = parentIdx;
+      parentIdx = this.getParentFromIndex(parentIdx);
+    }
+
+
   }
 
   heapifyDown() {
 
   }
+
+  getParentFromIndex(idx) {
+    return Math.floor((idx - 1) / 2);
+  }
 }
+
+let fart = new minHeap();
+console.log(fart.getParentFromIndex(4));
