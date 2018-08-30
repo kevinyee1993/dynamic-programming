41b7d318e5651860c4bd7c7d2675e72eb451dd68
Kevin Yee <kevinyee1993@gmail.com>
2018-08-29 17:56:51 -0700

    Start second solution without extra space

--- leetcode/duplicates_in_array.js
+++ leetcode/duplicates_in_array.js
@@ -1,3 +1,4 @@
+// O(n) time and O(n) space
 function findDuplicates(nums) {
   let answer = [];
   let counter = {};
@@ -14,3 +15,23 @@ function findDuplicates(nums) {
 
   return answer;
 }
+
+// time: O(n)
+// space: O(1) excluding the array created to store the answer
+// function findDuplicates2(nums) {
+//   let answer = [];
+//
+//   for(let i = 0; i < nums.length; i++) {
+//     let currNum = nums[i];
+//
+//     if(currNum < 0) {
+//       answer.push(i);
+//     } else {
+//       nums[currNum] *= -1;
+//     }
+//   }
+//
+//   return answer;
+// }
+
+// console.log(findDuplicates2([4,3,2,7,8,2,3,1]));
