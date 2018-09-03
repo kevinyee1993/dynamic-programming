1d09c6d9faa92f772014ecf8e672d1a158abc65c
Kevin Yee <kevinyee1993@gmail.com>
2018-09-03 15:27:41 -0700

    Finish monotonic array for both increase and decrease

--- leetcode/monotonic_increasing_array.js
+++ leetcode/monotonic_increasing_array.js
@@ -1,17 +1,24 @@
 function isMonotonic(A) {
 
-  // edge case
+  // edge case if length of array is just 1
   if(A.length <= 1) {
     return true;
   }
 
   let prevNum = A[0];
+  let trend;
 
-  let direction;
-
+  if(A[0] > A[A.length - 1]) {
+    trend = -1;
+  } else {
+    trend = 1;
+  }
 
   for(let i = 1; i < A.length; i++) {
-    if(prevNum > A[i]) {
+
+    // for the condition here, check if the array is trending
+    // up or down rather than just up
+    if( disobeysTrend(prevNum, A[i], trend) ) {
       return false;
     }
 
@@ -21,5 +28,13 @@ function isMonotonic(A) {
   return true;
 }
 
-const test = [1,2,2,3];
+function disobeysTrend(prevNum, currNum, trend) {
+  if(trend === 1) {
+    return (prevNum - currNum) > 0;
+  } else {
+    return (prevNum - currNum) < 0;
+  }
+}
+
+const test = [1,1,1,0];
 console.log(isMonotonic(test));
