db2db880970fe7fd3f32e336939850a0e48c6ae6
Kevin Yee <kevinyee1993@gmail.com>
2018-09-04 14:55:54 -0700

    Complete search bst

---
+++ leetcode/search_bst.js
@@ -0,0 +1,32 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = this.right = null;
+  }
+}
+
+function searchBST(root, val) {
+  if(!root) {
+    return null;
+  }
+
+  if(root.val === val) {
+    return root;
+  }
+
+  if(val > root.val) {
+    return searchBST(root.right, val);
+  } else if (val < root.val) {
+    return searchBST(root.left, val);
+  }
+
+}
+
+let a = new TreeNode(6);
+let b = new TreeNode(3);
+let c = new TreeNode(8);
+
+a.left = b;
+a.right = c;
+
+console.log(searchBST(a, 8));
