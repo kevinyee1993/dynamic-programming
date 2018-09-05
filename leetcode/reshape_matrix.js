4d63250d056975e3a5ddb62354d9f6ce0a2d5576
Kevin Yee <kevinyee1993@gmail.com>
2018-09-05 15:20:34 -0700

    Finish reshape matrix

--- leetcode/reshape_matrix.js
+++ leetcode/reshape_matrix.js
@@ -1,12 +1,19 @@
+// O(n) time
+// O(n) space
+
 function matrixReshape(nums, r, c) {
   let store = flatten(nums);
   let answer = createArray(r, c);
+  let counter = 0;
 
   for(let i = 0; i < r; i++) {
     for(let j = 0; j < c; j++) {
-
+      answer[i][j] = store[counter];
+      counter++;
     }
   }
+
+  return answer;
 }
 
 function createArray(r,c) {
@@ -31,5 +38,5 @@ function flatten(nums) {
   return store;
 }
 
-// let test = [[1,2,3],[4,5,6],[7,8,9]];
-// console.log(flatten(test));
+let test = [[1,2],[4,5],[7,8]];
+console.log(matrixReshape(test, 6, 1));
