b736de7c107fea2a9444d25436f90a8d474224db
Kevin Yee <kevinyee1993@gmail.com>
2018-09-11 14:03:43 -0700

    Finish recursively reverse linked list

--- leetcode/recursively_reverse_linked_list.js
+++ leetcode/recursively_reverse_linked_list.js
@@ -1,3 +1,6 @@
+// O(n) time complexity
+// O(1) space complexity
+
 class ListNode{
   constructor(value) {
     this.value = value;
@@ -6,5 +9,33 @@ class ListNode{
 }
 
 function reverseList(head) {
-  
+  if(head.next) {
+    reverseList(head.next);
+  }
+
+  console.log(head.value);
 }
+
+let a = new ListNode(1);
+let b = new ListNode(2);
+let c = new ListNode(3);
+let d = new ListNode(4);
+let e = new ListNode(5);
+let f = new ListNode(6);
+let g = new ListNode(7);
+let h = new ListNode(8);
+let i = new ListNode(9);
+let j = new ListNode(10);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+f.next = g;
+g.next = h;
+h.next = i;
+i.next = j;
+
+
+reverseList(a);
