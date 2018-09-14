a8dc03585c9c318590d43f3484f5622ef3eb0388
Kevin Yee <kevinyee1993@gmail.com>
2018-09-14 16:25:00 -0700

    Refresh max depth binary tree

--- leetcode/max_depth_binary_tree.js
+++ leetcode/max_depth_binary_tree.js
@@ -1,18 +1,19 @@
+// O(n) time where n is the depth? of the tree
+// O(n) space 
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+  constructor(value) {
+    this.value = value;
+    this.left = this.right = null;
   }
 }
 
-function maxDepthBinaryTree(root) {
-  if(root === null) {
+function maxDepth(root) {
+  if(!root) {
     return 0;
   }
 
-  let leftDepth = maxDepthBinaryTree(root.left);
-  let rightDepth = maxDepthBinaryTree(root.right);
+  let leftDepth = maxDepth(root.left);
+  let rightDepth = maxDepth(root.right);
   let bigDepth;
 
   if(leftDepth > rightDepth) {
@@ -21,5 +22,64 @@ function maxDepthBinaryTree(root) {
     bigDepth = rightDepth;
   }
 
-  return bigDepth + 1;
+  return 1 + bigDepth;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class TreeNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+//
+// function maxDepthBinaryTree(root) {
+//   if(root === null) {
+//     return 0;
+//   }
+//
+//   let leftDepth = maxDepthBinaryTree(root.left);
+//   let rightDepth = maxDepthBinaryTree(root.right);
+//   let bigDepth;
+//
+//   if(leftDepth > rightDepth) {
+//     bigDepth = leftDepth;
+//   } else {
+//     bigDepth = rightDepth;
+//   }
+//
+//   return bigDepth + 1;
+// }
