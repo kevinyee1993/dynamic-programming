09b15565b3e9980ecd7a070163f34e2c786436c4
Kevin Yee <kevinyee1993@gmail.com>
2018-09-20 16:37:10 -0700

    Start first common ancestor with nodes that have ref to parent node and create find depth function

--- leetcode/treesAndGraphs/first_common_ancestor.js
+++ leetcode/treesAndGraphs/first_common_ancestor.js
@@ -6,8 +6,17 @@ class TreeNode {
   }
 }
 
+// with reference to parent node
+class TreeNodeWithParent {
+  constructor(value) {
+    this.value = value;
+    this.parent = null;
+    this.left = this.right = null;
+  }
+}
+
 // time: O(nlog(n))
-// space: O(n) 
+// space: O(n)
 
 function firstCommonAncestor(head, node1, node2) {
 
@@ -53,9 +62,33 @@ let a = new TreeNode(1);
 let b = new TreeNode(2);
 let c = new TreeNode(3);
 let d = new TreeNode(4);
-
+//
 a.left = b;
 b.right = c;
 b.left = d;
 
-console.log(firstCommonAncestor(a, c, d));
+// console.log(firstCommonAncestor(a, c, d));
+
+function firstCommonAncestorWithParent(head, node1, node2) {
+
+}
+
+function findDepth(head) {
+  if(!head) {
+    return 0;
+  }
+
+  let leftDepth = findDepth(head.left);
+  let rightDepth = findDepth(head.right);
+  let bigDepth;
+
+  if(leftDepth > rightDepth) {
+    bigDepth = leftDepth;
+  } else {
+    bigDepth = rightDepth;
+  }
+
+  return 1 + bigDepth;
+}
+
+console.log(findDepth(a));
