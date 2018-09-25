65f4dbdad64a8c9370322330f5cc40ce8a792e26
Kevin Yee <kevinyee1993@gmail.com>
2018-09-25 15:27:07 -0700

    Start robot in a grid and outlined basic strategy

---
+++ leetcode/dynamicProgramming/robot_in_a_grid.js
@@ -0,0 +1,12 @@
+function robotGrid(grid) {
+  // at current position
+
+  // if current position is out of bounds or restricted, return false
+  // if current position is the target(bottom right index) return true
+
+  // if can move right, recursive call to move right
+  // if can move down, recursive call to move down
+
+
+  // return recursiveCall1 || recursiveCall2
+}
