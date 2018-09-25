25cf1e9508f3b4e6e647d7619d2b8e6da7acfb89
Kevin Yee <kevinyee1993@gmail.com>
2018-09-24 17:44:32 -0700

    Finish graph traversal problem

--- leetcode/treesAndGraphs/graph_traversal.js
+++ leetcode/treesAndGraphs/graph_traversal.js
@@ -1,6 +1,48 @@
 function findLongestColor(graph) {
   const visitedNodes = initializeVisitedHash(graph);
   const biggest = { count: 0, color: null };
+
+  for(let row = 0; row < graph.length; row++) {
+    for(let col = 0; col < graph[0].length; col++) {
+      if(!visitedNodes[row][col]) {
+        let currColor = graph[row][col];
+        let currCount = { };
+        currCount[currColor] = 1;
+        visitedNodes[row][col] = true;
+
+
+        traverseGraph(row, col+1, currCount, visitedNodes, currColor, graph);
+        traverseGraph(row, col-1, currCount, visitedNodes, currColor, graph);
+        traverseGraph(row+1, col, currCount, visitedNodes, currColor, graph);
+        traverseGraph(row-1, col, currCount, visitedNodes, currColor, graph);
+
+        if(currCount[currColor] > biggest.count) {
+          biggest.count = currCount[currColor];
+          biggest.color = currColor;
+        }
+      }
+    }
+  }
+
+  return biggest;
+}
+
+function traverseGraph(row, col, currCount, visitedNodes, prevColor, graph) {
+  if(!graph[row] || !graph[row][col]) {
+    return;
+  }
+
+  let currColor = graph[row][col];
+  if(!visitedNodes[row][col] && currColor === prevColor) {
+    visitedNodes[row][col] = true;
+    currCount[currColor]++;
+    traverseGraph(row, col+1, currCount, visitedNodes, currColor, graph);
+    traverseGraph(row, col-1, currCount, visitedNodes, currColor, graph);
+    traverseGraph(row+1, col, currCount, visitedNodes, currColor, graph);
+    traverseGraph(row-1, col, currCount, visitedNodes, currColor, graph);
+  } else {
+    return;
+  }
 }
 
 function initializeVisitedHash(graph) {
@@ -12,3 +54,11 @@ function initializeVisitedHash(graph) {
 
   return visitedNodes;
 }
+
+let graph = [
+  ['g','g','b','r'],
+  ['g','b','r','b'],
+  ['r','b','b','b']
+];
+
+console.log(findLongestColor(graph));
