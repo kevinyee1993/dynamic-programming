dc162a4062f9e47fbf948eaf9f1559bc4bfc1fd7
Kevin Yee <kevinyee1993@gmail.com>
2018-09-25 17:14:01 -0700

    Complete robot in a grid initial solution

--- leetcode/dynamicProgramming/robot_in_a_grid.js
+++ leetcode/dynamicProgramming/robot_in_a_grid.js
@@ -6,10 +6,12 @@ function robotGrid(grid) {
   let targetRow = grid.length - 1;
   let targetCol = grid[0].length - 1;
 
+  let path = [];
+
   // if current position is out of bounds or restricted, return false
   // if current position is the target(bottom right index) return true
-  let downCheck = helper(currRow + 1, currCol, targetRow, targetCol, grid);
-  let rightCheck = helper(currRow, currCol + 1, targetRow, targetCol, grid);
+  let downCheck = helper(currRow + 1, currCol, targetRow, targetCol, grid, path, "down");
+  let rightCheck = helper(currRow, currCol + 1, targetRow, targetCol, grid, path, "right");
 
 
   // if can move right, recursive call to move right
@@ -21,31 +23,34 @@ function robotGrid(grid) {
 
 // right now this is just checking if a path exists,
 // need to also return the path itself
-function helper(currRow, currCol, targetRow, targetCol, grid) {
+function helper(currRow, currCol, targetRow, targetCol, grid, path, direction) {
   if(currRow === targetRow && currCol === targetCol) {
-    return true;
+    path.push(direction);
+    return path;
   }
 
   if(currRow > grid.length - 1 || currCol > grid[0].length - 1) {
-    return false;
+    return null;
   }
 
   let currPos = grid[currRow][currCol];
 
-  if(!currPos || currPos === null) {
-    return false;
+  if(!currPos || currPos === 0) {
+    return null;
   }
 
-  let downCheck = helper(currRow + 1, currCol, targetRow, targetCol, grid);
-  let rightCheck = helper(currRow, currCol + 1, targetRow, targetCol, grid);
+  path.push(direction);
+
+  let downCheck = helper(currRow + 1, currCol, targetRow, targetCol, grid, Array.from(path), "down");
+  let rightCheck = helper(currRow, currCol + 1, targetRow, targetCol, grid, Array.from(path), "right");
 
   return downCheck || rightCheck;
 }
 
 let grid = [
   [1,1,1],
-  [null,1,null],
-  [1,null,1]
+  [0,1,1],
+  [1,0,1]
 ];
 
 console.log(robotGrid(grid));
