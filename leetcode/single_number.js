e93ad899a2b53c409f40b997377318ff706bbf00
Kevin Yee <kevinyee1993@gmail.com>
2018-09-27 19:25:37 -0700

    Complete single number in linear time no extra space

---
+++ leetcode/single_number.js
@@ -0,0 +1,12 @@
+// O(n) time and O(1) space
+function findSingleNumber(arr) {
+  let store = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    store ^= arr[i];
+  }
+
+  return store;
+}
+
+console.log(findSingleNumber([4,1,2,1,2,4,5]));
