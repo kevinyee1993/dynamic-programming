e5003e044bea7d69ffb909e506fa57d4ce181cc1
Kevin Yee <kevinyee1993@gmail.com>
2018-12-03 23:28:20 -0800

    Start queue reconstruction by height

---
+++ leetcode/queue_reconstruction_by_height.js
@@ -0,0 +1,17 @@
+var reconstructQueue = function(people) {
+
+  people.sort( (a,b) => {
+    return a[0] < b[0];
+  });
+
+
+
+};
+
+const input = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]];
+
+input.sort( (a,b) => {
+  return a[0] < b[0];
+});
+
+console.log(input);
