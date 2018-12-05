c73b18046e6ec9ece6460022e54dbce75b3b15c9
Kevin Yee <kevinyee1993@gmail.com>
2018-12-04 20:42:36 -0800

    Fix

--- leetcode/queue_reconstruction_by_height.js
+++ leetcode/queue_reconstruction_by_height.js
@@ -15,32 +15,3 @@ input.sort( (a,b) => {
 });
 
 console.log(input);
-
-/*
-things to ask / info for alto pharmacy
-
-20 min intro call
-learn about interests and make sure what
-they're looking for is aligned with what theyre offering
-this role is frontend/backend a lot of bug fixing issues
-
-what piqued interest in alto
-think about what you didnt like about cvs and how alto fixes
-lady who couldn't come in to store but we can't bring it outside
-
-what am i most proud of in career
-talk her through why youre proud of it
-communication is important
-
-what questions do you have -> use every minute available
-ask about the company
-growth - team or business oriented
-
-putting the patient first
-
-
-what programming languages to use in tech interview
-
-
-
-*/
