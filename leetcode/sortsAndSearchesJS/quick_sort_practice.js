1a97febcebd127011f1dcb0a3706b39ddb41da86
Kevin Yee <kevinyee1993@gmail.com>
2018-12-09 18:43:29 -0800

    Quick sort practice

--- leetcode/sortsAndSearchesJS/quick_sort_practice.js
+++ leetcode/sortsAndSearchesJS/quick_sort_practice.js
@@ -1,36 +1,105 @@
-function quickSort(arr, left, right) {
-  if(left >= right) {
+function quickSort(arr) {
+  helper(arr, 0, arr.length - 1);
+}
+
+function helper(arr, start, end) {
+  if(start >= end) {
     return arr;
   }
 
-  let pivot = arr[right];
-  let partitionIndex = partition(arr, left, right, pivot);
+  let pivot = arr[end];
+  let partitionIdx = partition(arr, start, end, pivot);
 
-  quickSort(arr, left, partitionIndex - 1);
-  quickSort(arr, partitionIndex + 1, right);
+  helper(arr, start, partitionIdx - 1);
+  helper(arr, partitionIdx + 1, end);
 }
 
-function partition(arr, left, right, pivot) {
-  let partitionIndex = left;
+function partition(arr, start, end, pivot) {
+  let partitionIdx = start;
 
-  for(let i = left; i < right; i++) {
-    if(arr[i] < pivot) {
-      swap(arr, i, partitionIndex);
-      partitionIndex++;
+  for(let i = start; i < end; i++) {
+    if(arr[i] <= pivot) {
+      swap(arr, i, partitionIdx);
+      partitionIdx++;
     }
   }
 
-  swap(arr, partitionIndex, right);
+  swap(arr, partitionIdx, end);
 
-  return partitionIndex;
+  return partitionIdx;
 }
 
 function swap(arr, idx1, idx2) {
-  const temp = arr[idx1];
+  let temp = arr[idx1];
   arr[idx1] = arr[idx2];
   arr[idx2] = temp;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function quickSort(arr, left, right) {
+//   if(left >= right) {
+//     return arr;
+//   }
+//
+//   let pivot = arr[right];
+//   let partitionIndex = partition(arr, left, right, pivot);
+//
+//   quickSort(arr, left, partitionIndex - 1);
+//   quickSort(arr, partitionIndex + 1, right);
+// }
+//
+// function partition(arr, left, right, pivot) {
+//   let partitionIndex = left;
+//
+//   for(let i = left; i < right; i++) {
+//     if(arr[i] < pivot) {
+//       swap(arr, i, partitionIndex);
+//       partitionIndex++;
+//     }
+//   }
+//
+//   swap(arr, partitionIndex, right);
+//
+//   return partitionIndex;
+// }
+//
+// function swap(arr, idx1, idx2) {
+//   const temp = arr[idx1];
+//   arr[idx1] = arr[idx2];
+//   arr[idx2] = temp;
+// }
+
 let a = [5,1,643,2,53,1,97,5,8];
-quickSort(a, 0, a.length - 1);
+// quickSort(a, 0, a.length - 1);
+quickSort(a);
 console.log(a);
